bed0ca706f2113ca2e3a477c2b564a75447b0f91
황종수 <jongsuhwang@hansol.com>
2024-11-20 14:16:16 +0900

    🎉 First commit

---
+++ viewers/Webviewer.tsx
@@ -0,0 +1,33 @@
+import {StyleSheet, Dimensions, View} from 'react-native';
+import {WebView} from 'react-native-webview';
+import React from 'react';
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+
+const Webviewer: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{uri: 'https://food-diary-window.vercel.app/diary'}}
+        // source={{uri: 'http://localhost:5173/report'}}
+        style={styles.webview}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  webview: {
+    flex: 1,
+    width: deviceWidth,
+    height: deviceHeight,
+  },
+});
+
+export default Webviewer;
